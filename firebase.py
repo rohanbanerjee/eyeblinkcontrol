e1dee013aba7ae678ea07a811db79d281d704367
Rohan Banerjee <banerjee.rohan98@gmail.com>
2018-03-14 16:14:17 +0530

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -7,10 +7,10 @@ def motiondetect(a):
 
 
     config = {
-    "apiKey": "AAAATN-ZpOw:APA91bFdXet6yxNzAnifpZ4resbb66YERmgP-vAADNA_Knn2bHLVO-aDeQQuL9q2ibMD9PYP-Hyk3-TaKPiZKZiFlC-SiJWY_bBliNq-AoyoiPM9KVvmuk-uve4-zKwAntbJTEVnblOQ",
-    "authDomain": "motion-sensor-39267.firebaseapp.com",
-    "databaseURL": "https://motion-sensor-39267.firebaseio.com",
-    "storageBucket": "motion-sensor-39267.appspot.com"
+    "apiKey": "enter yor data",
+    "authDomain": "enter your data",
+    "databaseURL": "enter your data",
+    "storageBucket": "enter your data"
     }
 
     firebase = pyrebase.initialize_app(config)
